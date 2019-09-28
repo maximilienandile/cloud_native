af3057dad8d1991c880f1b760a6ab03a30de5d4e
Maximilien Andile <maximilien.andile.compere@gmail.com>
2019-09-28 10:23:48 +0200

    initial

---
+++ cmd/paris/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+  "net/http"
+
+  "github.com/gorilla/mux"
+)
+
+func main() {
+
+  r := mux.NewRouter()
+  server := http.Server{
+    Addr:    ":8080",
+    Handler: r,
+  }
+
+  diag := http.Server{
+    Addr: ":8081",
+  }
+
+  go func() {
+    server.ListenAndServe()
+  }()
+
+  diag.ListenAndServe()
+}
